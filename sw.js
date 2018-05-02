9ffcc130c3e66ba9ff7882a3c4684fab0ebb3600
sg <396857483@qq.com>
2018-05-02 15:10:38 +0800

    created pwa

---
+++ sw.js
@@ -0,0 +1,40 @@
+var cacheStorageKey = "pwa";
+
+var cacheList = ['/', 'index.html','icon.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheStorageKey).then(function (cache) {
+      cache.addAll(cacheList);
+    }).then(function () {
+      self.skipWaiting();
+    })
+  )
+})
+
+self.addEventListener('fetch', function (e) {
+  e.repondWith(
+    caches.match(e.request).then(function (response) {
+      if (response != null) {
+        return response;
+      }
+      return fetch(e.reponse.url);
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheNames) {
+          return cacheNames !== cacheStorageKey;
+        }).map(function (cacheNames) {
+          return caches.delete(cacheNames);
+        })
+      )
+    }).then(function(){
+      return self.clients.claim();
+    })
+  )
+})
